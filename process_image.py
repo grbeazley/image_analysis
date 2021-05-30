82c8863af49cc81c089286f0b9bcd0fe1dc6db6c
grbeazley <grbeazley@gmail.com>
2021-05-30 18:00:43 +0100

    Added mixing

--- process_image.py
+++ process_image.py
@@ -7,26 +7,54 @@ import skimage.feature
 from matplotlib.widgets import Slider
 
 # Adds Fiji colour map
-util.register_fiji_cmap('imageJ-red-black')
+util.register_fiji_cmap_red('imageJ-red-black')
+util.register_fiji_cmap_green('imageJ-green-black')
 
 # Read LSM image
-raw_image_data = read_lsm_image('4273', 'image 23.lsm', stack=0, channel=0)
+folder_name = '4275'
+image_name = 'image 21.lsm'
+
+raw_image_data_red = read_lsm_image(folder_name, image_name, stack=0, channel=0)
+raw_image_data_green = read_lsm_image(folder_name, image_name, stack=0, channel=1)
 
 # Put pixel values into the range 0 to 256
-image_data = (raw_image_data / np.max(raw_image_data)) * 256
+image_data_red = (raw_image_data_red / np.max(raw_image_data_red)) * 256
+image_data_green = (raw_image_data_green / np.max(raw_image_data_green)) * 256
+
+# image_data = image_data_red
+
+# Open UI to adjust mixing
+util.mix_images(image_data_red, image_data_green)
+# plt.figure()
+# plt.imshow(image_data_red, cmap='imageJ-red-black')
+# plt.show()
+# plt.figure()
+# plt.imshow(image_data_green, cmap='imageJ-green-black')
+# plt.show()
+
+# plt.figure()
+# plt.imshow(np.abs(image_data_red - image_data_green), cmap='imageJ-green-black')
+# plt.show()
+
+fact = 1
+image_data = image_data_red - (image_data_green * fact)
+
+# plt.figure()
+# plt.hist(image_data.flatten(), bins=100)
+# plt.show()
 
 # Open UI to adjust image properties
-util.adjustable_image(image_data)
-
-# Set image values & adjust image
-brightness = 0
-mask = 34
-contrast = 1.01
-image_adjusted = np.clip(util.adjust_contrast(image_data * (image_data >= mask), contrast=contrast)
-                         + brightness, 0, 256)
+# util.adjustable_image(image_data)
 
 # Run edge detection on corrected image
-#util.adjustable_detector(image_adjusted)
+# util.adjustable_detector(image_data)
+
+# Set image values & adjust image
+brightness = 47
+mask = 22
+contrast = 1.33
+image_adjusted = np.clip(util.adjust_contrast(image_data * (image_data >= mask), contrast=contrast)
+                         + brightness, 0, 256)
 
 sigma = 3
 area = 25
--- utilities.py
+++ utilities.py
@@ -6,7 +6,7 @@ from skimage import measure, exposure
 import skimage.feature
 
 
-def register_fiji_cmap(name):
+def register_fiji_cmap_red(name):
     # Create a color map to match Fiji/ImageJ
     cdict = {'red':   ((0.0,  0.0, 0.0),
                        (0.5,  0.5, 0.5),
@@ -24,49 +24,94 @@ def register_fiji_cmap(name):
     plt.register_cmap(cmap=test_red_black)
 
 
+def register_fiji_cmap_green(name):
+    cdict = {'red':   ((0.0,  0.0, 0.0),
+                       (0.5,  0.0, 0.0),
+                       (1.0,  1.0, 1.0)),
+
+             'green': ((0.0,  0.0, 0.0),
+                       (0.5,  0.5, 0.5),
+                       (1.0,  1.0, 1.0)),
+
+             'blue':  ((0.0,  0.0, 0.0),
+                       (0.5,  0.0, 0.0),
+                       (1.0,  0.0, 0.0))}
+
+    test_green_black = LinearSegmentedColormap(name, cdict)
+    plt.register_cmap(cmap=test_green_black)
+
+
 def adjust_contrast(image, contrast=1):
     mid = (np.max(image) + np.min(image)) / 2
     return ((image - mid) * contrast) + mid
 
 
-def adjustable_image(image):
+def mix_images(image_red, image_green):
     fig = plt.figure()
     ax = fig.add_subplot(111)
     fig.subplots_adjust(bottom=0.25)
-    min0 = 1
-    max0 = 0
-    brg0 = 0
+    fact = 1
 
+    # image = ((image_red - image_green) + 256) / 2
+    image = np.where(image_red - (image_green) < 0, 0, image_red - (image_green))
     im1 = ax.imshow(image, cmap='imageJ-red-black')
     fig.colorbar(im1)
 
     axcolor = 'lightgoldenrodyellow'
-    axmin = fig.add_axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-    axmax = fig.add_axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
-    axbrg = fig.add_axes([0.25, 0.2, 0.65, 0.03], facecolor=axcolor)
+    axfact = fig.add_axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
 
-    smin = Slider(axmin, 'Contrast', 0, 2, valinit=min0, valstep=0.01)
-    smax = Slider(axmax, 'Mask', 0, 256, valinit=max0, valstep=1)
-    sbrg = Slider(axbrg, 'Brightness', -128, 128, valinit=brg0, valstep=1)
+    sfact = Slider(axfact, 'Factor', 0, 2, valinit=fact, valstep=0.01)
 
     def _update(val):
-        print('test')
-        update_im = np.clip(adjust_contrast(image * (image >= smax.val), contrast=smin.val) + sbrg.val, 0, 256)
+        print(sfact.val)
+        update_im = np.where(image_red - (image_green * sfact.val) < 0, 0, image_red - (image_green * sfact.val))
+        # update_im = np.clip(image_red - (image_green * sfact.val), a_min=0, a_max=256)
         im1.set_data(update_im)
         fig.canvas.draw()
 
-    smin.on_changed(_update)
-    smax.on_changed(_update)
-    sbrg.on_changed(_update)
+    sfact.on_changed(_update)
     plt.show()
+
+#
+# def adjustable_image(image):
+#     fig = plt.figure()
+#     ax = fig.add_subplot(111)
+#     fig.subplots_adjust(bottom=0.25)
+#     min0 = 1
+#     max0 = 0
+#     brg0 = 0
+#
+#     im1 = ax.imshow(image, cmap='imageJ-red-black')
+#     fig.colorbar(im1)
+#
+#     axcolor = 'lightgoldenrodyellow'
+#     axmin = fig.add_axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+#     axmax = fig.add_axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+#     axbrg = fig.add_axes([0.25, 0.2, 0.65, 0.03], facecolor=axcolor)
+#
+#     smin = Slider(axmin, 'Contrast', 0, 2, valinit=min0, valstep=0.01)
+#     smax = Slider(axmax, 'Mask', 0, 256, valinit=max0, valstep=1)
+#     sbrg = Slider(axbrg, 'Brightness', -128, 128, valinit=brg0, valstep=1)
+#
+#
+#
+#
+#     smin.on_changed(_update)
+#     smax.on_changed(_update)
+#     sbrg.on_changed(_update)
+#     plt.show()
 
 
 def adjustable_detector(image):
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    fig.subplots_adjust(bottom=0.25)
+    fig.subplots_adjust(bottom=0.35)
     sig0 = 3
     size0 = 25
+
+    min0 = 1
+    max0 = 0
+    brg0 = 0
 
     im1 = ax.imshow(image, cmap='imageJ-red-black')
     edges0 = skimage.feature.canny(image=image, sigma=sig0)
@@ -79,13 +124,22 @@ def adjustable_detector(image):
     outlines = ax.scatter(all_coords[:, 1], all_coords[:, 0], 0.1)
 
     axcolor = 'lightgoldenrodyellow'
-    axsig = fig.add_axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
-    axsize = fig.add_axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+    axsig = fig.add_axes([0.25, 0.25, 0.65, 0.03], facecolor=axcolor)
+    axsize = fig.add_axes([0.25, 0.3, 0.65, 0.03], facecolor=axcolor)
+
+    axmin = fig.add_axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+    axmax = fig.add_axes([0.25, 0.15, 0.65, 0.03], facecolor=axcolor)
+    axbrg = fig.add_axes([0.25, 0.2, 0.65, 0.03], facecolor=axcolor)
 
     ssig = Slider(axsig, 'Sigma', 0.1, 10, valinit=sig0, valstep=0.1)
     ssize = Slider(axsize, 'Size', 0, 100, valinit=size0, valstep=1)
 
+    smin = Slider(axmin, 'Contrast', 0, 2, valinit=min0, valstep=0.01)
+    smax = Slider(axmax, 'Mask', 0, 256, valinit=max0, valstep=1)
+    sbrg = Slider(axbrg, 'Brightness', -128, 128, valinit=brg0, valstep=1)
+
     def _update(val):
+        update_im = np.clip(adjust_contrast(image * (image >= smax.val), contrast=smin.val) + sbrg.val, 0, 256)
         edges = skimage.feature.canny(image=image, sigma=ssig.val)
         segs = measure.regionprops(measure.label(edges))
         all_coords = np.zeros([1, 2])
@@ -94,9 +148,14 @@ def adjustable_detector(image):
                 all_coords = np.concatenate([seg.coords, all_coords])
 
         outlines.set_offsets(np.flip(all_coords))
+        im1.set_data(update_im)
         fig.canvas.draw()
+
 
     ssig.on_changed(_update)
     ssize.on_changed(_update)
+    smin.on_changed(_update)
+    smax.on_changed(_update)
+    sbrg.on_changed(_update)
     plt.show()
 
